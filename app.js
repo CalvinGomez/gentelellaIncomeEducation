7788588823637969d05800c8f9daee105ff32b77
Alisa Prathnadi <aprathna@ucsd.edu>
2016-06-02 11:48:16 -0700

    added splash

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/about', routes);
+app.use('/navigation', routes);
+app.use('/splash', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Express' });
 });
 
+router.get('/navigation', function(req, res, next) {
+  res.render('navigation', { title: 'Express' });
+});
+
+router.get('/splash', function(req, res, next) {
+  res.render('splash', { title: 'Express' });
+});
+
 module.exports = router;
